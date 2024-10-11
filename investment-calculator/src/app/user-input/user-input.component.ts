318e9019ef34c269bdfe69b0044fbec2727b9ce6
Petar Stojkovski <petars089@gmail.com>
2024-10-11 10:12:22 +0200

    feat(investment-calculator): Used a service for cross component communication

--- investment-calculator/src/app/user-input/user-input.component.ts
+++ investment-calculator/src/app/user-input/user-input.component.ts
@@ -1,6 +1,6 @@
-import { Component, output, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { InvestmentInput } from '../investment-input.model';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
@@ -10,24 +10,24 @@ import { InvestmentInput } from '../investment-input.model';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-  calculate = output<InvestmentInput>();
-
   enteredInitialInvestment = signal('0');
   enteredAnnualInvestment =  signal('0');
   enteredExpectedReturn = signal('5');
   enteredDuration = signal('10');
 
-  onSubmit() {
-   this.calculate.emit({
-     initialInvestment: +this.enteredInitialInvestment(),
-     duration: +this.enteredDuration(),
-     expectedReturn: +this.enteredExpectedReturn(),
-     annualInvestment: +this.enteredAnnualInvestment()
-   })
+  constructor(private investmentService: InvestmentService) {}
 
-   this.enteredAnnualInvestment.set('0');
-   this.enteredInitialInvestment.set('0');
-   this.enteredExpectedReturn.set('5');
-   this.enteredDuration.set('10');
+  onSubmit() {
+    this.investmentService.calculateInvestmentResults({
+      initialInvestment: +this.enteredInitialInvestment(),
+      duration: +this.enteredDuration(),
+      expectedReturn: +this.enteredExpectedReturn(),
+      annualInvestment: +this.enteredAnnualInvestment()
+    });
+
+    this.enteredAnnualInvestment.set('0');
+    this.enteredInitialInvestment.set('0');
+    this.enteredExpectedReturn.set('5');
+    this.enteredDuration.set('10');
   }
 }
